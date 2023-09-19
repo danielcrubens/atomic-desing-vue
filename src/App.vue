ccad3c783133d78e6b060322169231ed1ed198fa
danielcrubens <dcr.tecnologi@gmail.com>
2023-09-19 16:05:19 -0300

    molécula de serviço :fire:

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div class=" components">
-<SocialNetworks/>
+<Service title="Front End" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur temporibus, soluta itaque, corporis sint ratione eum atque magni, dolorem labore error non nemo debitis placeat natus? In enim asperiores sunt?"/>
+
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SocialNetworks } from '@/components/molecules'
+import { Service } from '@/components/molecules'
 
 
 export default defineComponent({
-  components: {
-    SocialNetworks    
-  }
+  components: {Service}
 })
 </script>
 <style lang="scss" scoped>
