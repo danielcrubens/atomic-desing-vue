70888d9fd4446babd81e69e24e1b33c942d11db1
danielcrubens <dcr.tecnologi@gmail.com>
2023-09-19 10:27:15 -0300

    molécula de busca :fire:

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div class=" components">
-    <Logo />
-    <NavBar />
+    <SearchForm />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Logo } from './components/atoms'
-import { NavBar } from './components/molecules'
+import { SearchForm } from './components/molecules'
 
 
 export default defineComponent({
   components: {
-    Logo,
-    NavBar,
+    SearchForm
   }
 })
 </script>
 <style lang="scss" scoped>
-.component {}
 </style>
