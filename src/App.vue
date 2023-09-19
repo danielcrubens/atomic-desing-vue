5187076bfcffe0dbd284a62d00280303f3a81d90
danielcrubens <dcr.tecnologi@gmail.com>
2023-09-19 10:11:30 -0300

    átomo de logo e molécula de navegação :fire:

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-<div>
-
-</div>
+  <div class=" components">
+    <Logo />
+    <NavBar />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { Logo } from './components/atoms'
+import { NavBar } from './components/molecules'
+
 
 export default defineComponent({
-
+  components: {
+    Logo,
+    NavBar,
+  }
 })
 </script>
+<style lang="scss" scoped>
+.component {}
+</style>
